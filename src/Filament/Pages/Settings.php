020c5a8791bfe2917470cc69b94aa3a016202feb
“ianstudios” <ianstudioscom@gmail.com>
2025-08-26 17:09:58 +0700

    fix Settings

--- src/Filament/Pages/Settings.php
+++ src/Filament/Pages/Settings.php
@@ -88,7 +88,18 @@ class Settings extends Page
         try {
             $this->callHook('beforeValidate');
 
-            $data = Arr::dot($this->form->getState());
+            $fields = collect($this->form->getFlatFields(true));
+            $fieldsWithNestedFields = $fields->filter(fn (Field $field) => count($field->getChildComponents()) > 0);
+
+            $fieldsWithNestedFields->each(function (Field $fieldWithNestedFields, string $fieldWithNestedFieldsKey) use (&$fields) {
+                $fields = $fields->reject(function (Field $field, string $fieldKey) use ($fieldWithNestedFields, $fieldWithNestedFieldsKey) {
+                    return Str::startsWith($fieldKey, $fieldWithNestedFieldsKey . '.');
+                });
+            });
+
+            $data = $fields->mapWithKeys(function (Field $field, string $fieldKey) {
+                return [$fieldKey => data_get($this->form->getState(), $fieldKey)];
+            })->toArray();
 
             $this->callHook('afterValidate');
 
