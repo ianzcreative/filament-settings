b82a8b75c6fa1c528d965a8fef6282aea03ba2f5
“ianstudios” <ianstudioscom@gmail.com>
2025-09-04 09:47:01 +0700

    moved button to top-right

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,8 +1,9 @@
 <x-filament::page>
 	<x-filament-panels::form :wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()" wire:submit="save">
-		{{ $this->form }}
 
-		<x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
+		<x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" alignment="right" direction="row"/>
+		
+		{{ $this->form }}
 	</x-filament-panels::form>
 
 	<x-filament-panels::page.unsaved-data-changes-alert />
